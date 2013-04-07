32d0d5adfc249f632b1a0a8e5ead917890de0b5e
olmjo <jpolmsted@gmail.com>
2013-04-07 11:38:50 -0400

    add single-node Matlab example

---
+++ examples/ex5/ex5.m
@@ -0,0 +1,20 @@
+MC = 10 ;
+DUR = 20 ;
+matlabpool(getenv('PROCS')) ;
+
+tic ;
+for i = 1:MC
+  pause(DUR / MC) ;
+end
+st = toc ;
+disp(st) ;
+
+tic ;
+parfor i = 1:MC
+  pause(DUR / MC) ;
+end
+pt = toc ;
+disp(pt) ;
+
+exit ;
+
