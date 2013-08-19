222e03fa13360df1b6b86268f1e1d8bed6d7458e
olmjo <jpolmsted@gmail.com>
2013-08-19 13:29:07 -0400

    add repo selection for unattended installs

--- setup/install_misc.R
+++ setup/install_misc.R
@@ -1,3 +1,9 @@
+local({r <- getOption("repos");
+       r["CRAN"] <- "http://cran.r-project.org";
+       options(repos=r)
+   }
+      )
+
 install.packages(c("doMPI",
                    "foreach",
                    "snow",
