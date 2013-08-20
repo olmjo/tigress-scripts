94e5357867deefe6aa9d75ec445a40b33153c1fd
olmjo <jpolmsted@gmail.com>
2013-08-20 19:01:15 -0400

    use better source, include source of source;

--- test/hello.c
+++ test/hello.c
@@ -1,14 +1,17 @@
-/*The Parallel Hello World Program*/
+// Taken from <http://www.slac.stanford.edu/comp/unix/farm/mpi.html>
 #include <stdio.h>
 #include <mpi.h>
 
-int main (int argc, char **argv) {
-  int node ;
-  
-  MPI_Init(&argc,&argv) ;
-  MPI_Comm_rank(MPI_COMM_WORLD, &node) ;
-  
-  printf("Hello World from Node %d\n",node) ;
-  
-  MPI_Finalize() ;
+int main(int argc, char *argv[]) {
+    int numprocs, rank, namelen ;
+    char processor_name[MPI_MAX_PROCESSOR_NAME] ;
+    
+    MPI_Init(&argc, &argv) ;
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs) ;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank) ;
+    MPI_Get_processor_name(processor_name, &namelen) ;
+    
+    printf("Process %d on %s out of %d\n", rank, processor_name, numprocs) ;
+    
+    MPI_Finalize() ;
 }
