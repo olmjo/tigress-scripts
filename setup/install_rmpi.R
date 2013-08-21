559378e01b1a1c19688b637c0eb566424e7c390b
olmjo <jpolmsted@gmail.com>
2013-08-21 16:42:53 -0400

    use newer openmpi

--- setup/install_rmpi.R
+++ setup/install_rmpi.R
@@ -1,7 +1,7 @@
 install.packages("rmpi.tar.gz",
                  configure.args="
---with-Rmpi-include=/usr/local/openmpi/1.4.5/gcc/x86_64/include/
---with-Rmpi-libpath=/usr/local/openmpi/1.4.5/gcc/x86_64/lib64/
+--with-Rmpi-include=/usr/local/openmpi/1.6.3/gcc/x86_64/include/
+--with-Rmpi-libpath=/usr/local/openmpi/1.6.3/gcc/x86_64/lib64/
 --with-Rmpi-type=OPENMPI
 "
                  )
