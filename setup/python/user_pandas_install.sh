a0ee5a75057df06113453d8a9f24aa38c7a4bf60
olmjo <jpolmsted@gmail.com>
2014-01-08 15:04:04 -0500

    use python2.7

--- setup/python/user_pandas_install.sh
+++ setup/python/user_pandas_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-instdir=~/local/lib/python2.6/site-packages/
+module load python/2.7
+echo -e 'module load python/2.7\n'
+instdir=~/local/lib/python2.7/site-packages/
 mkdir -p ${instdir}
-echo -e PYTHONPATH=${instdir}:${PYTHONPATH} >> ~/.bashrc
-echo -e export PYTHONPATH  >> ~/.bashrc
+echo -e 'PYTHONPATH=${instdir}:${PYTHONPATH}\n' >> ~/.bashrc
+echo -e 'export PYTHONPATH\n'  >> ~/.bashrc
 . ~/.bashrc
 easy_install --prefix=$HOME/local pandas
 
