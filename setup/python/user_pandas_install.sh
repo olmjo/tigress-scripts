3fa089cb7ee1ad0614d39a738c5f78dd07f6c047
Jonathan Olmsted <jolmsted@princeton.edu>
2014-02-28 16:03:38 -0500

    fix quotes issue

--- setup/python/user_pandas_install.sh
+++ setup/python/user_pandas_install.sh
@@ -3,7 +3,8 @@ module load python/2.7
 echo -e 'module load python/2.7\n'
 instdir=~/local/lib/python2.7/site-packages/
 mkdir -p ${instdir}
-echo -e 'PYTHONPATH=${instdir}:${PYTHONPATH}\n' >> ~/.bashrc
+echo -e PYTHONPATH=${instdir}:${PYTHONPATH} >> ~/.bashrc
+echo -e '\n'
 echo -e 'export PYTHONPATH\n'  >> ~/.bashrc
 . ~/.bashrc
 easy_install --prefix=$HOME/local pandas
