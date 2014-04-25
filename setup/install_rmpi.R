301f347994de3a5ee5cb2ea6603e2f68bd86b1aa
Jonathan P. Olmsted <jpolmsted@gmail.com>
2014-04-25 16:55:05 -0400

    add autodetection of host and conditional usage of openmpi versions

--- setup/install_rmpi.R
+++
@@ -1,7 +0,0 @@
-install.packages("rmpi.tar.gz",
-                 configure.args="
---with-Rmpi-include=/usr/local/openmpi/1.4.5/gcc/x86_64/include/
---with-Rmpi-libpath=/usr/local/openmpi/1.4.5/gcc/x86_64/lib64/
---with-Rmpi-type=OPENMPI
-"
-                 )
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,12 +1,30 @@
+#!/bin/bash
 . /etc/profile.d/modules.sh
 
+LOCATION=$(hostname)
+RMPIVER=0.6-3
+
+case ${LOCATION} in
+    della4)
+        export OMPIVERS=1.6.5
+        echo "Using openmpi version ${OMPIVERS}."
+        ;;
+    della3)
+        export OMPIVERS=1.4.5
+        echo "Using openmpi version ${OMPIVERS}."
+        ;;
+    *)
+        export OMPIVERS=1.4.5
+        echo "Using openmpi version ${OMPIVERS}."
+        ;;
+esac
 
 
 while true; do
     read -p "Do you need to DL Rmpi? [y/n]" yn
     case $yn in
-        [Yy]* ) 
-            wget http://www.stats.uwo.ca/faculty/yu/Rmpi/download/linux/Rmpi_0.6-3.tar.gz -O rmpi.tar.gz; 
+        [Yy]* )
+            wget http://www.stats.uwo.ca/faculty/yu/Rmpi/download/linux/Rmpi_${RMPIVER}.tar.gz -O rmpi.tar.gz;
             break;;
 
         [Nn]* ) break;;
@@ -14,17 +32,17 @@ while true; do
     esac
 done
 
-module load openmpi/gcc/1.4.5/64
+module load openmpi/gcc/${OMPIVERS}/64
 
 while true; do
     read -p "Do you need to add openmpi libs to the linker path? [y/n]" yn
     case $yn in
         [Yy]* )
-            echo -e "LD_LIBRARY_PATH=/usr/local/openmpi/1.4.5/gcc/x86_64/lib64\n" >> ~/.bashrc;
+            echo -e "LD_LIBRARY_PATH=/usr/local/openmpi/${OMPIVERS}/gcc/x86_64/lib64\n" >> ~/.bashrc;
             echo -e "export LD_LIBRARY_PATH\n" >> ~/.bashrc;
             break;;
 
-        [Nn]* ) 
+        [Nn]* )
             break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -35,11 +53,14 @@ ulimit -l unlimited
 while true; do
     read -p "Do you need to install Rmpi? [y/n]" yn
     case $yn in
-        [Yy]* ) 
-            Rscript install_rmpi.R ; 
+        [Yy]* )
+            R CMD INSTALL --configure-args="--with-Rmpi-include=/usr/local/openmpi/${OMPIVERS}/gcc/x86_64/include
+                --with-Rmpi-libpath=/usr/local/openmpi/${OMPIVERS}/gcc/x86_64/lib64
+                --with-Rmpi-type=OPENMPI" \
+                    rmpi.tar.gz
             break;;
 
-        [Nn]* ) 
+        [Nn]* )
             break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -53,14 +74,12 @@ source ~/.bashrc
 while true; do
     read -p "Do you need to misc. HPC packages? [y/n]" yn
     case $yn in
-        [Yy]* ) 
-            Rscript install_misc.R; 
+        [Yy]* )
+            Rscript install_misc.R;
             break;;
 
-        [Nn]* ) 
+        [Nn]* )
             break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
-
-
