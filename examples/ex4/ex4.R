d8d0e789edaf86f2f642038b7b48d1724ae444cd
Jonathan P. Olmsted <jpolmsted@gmail.com>
2014-05-14 14:08:47 -0400

    add slurm support for array example

--- examples/ex4/ex4.R
+++ examples/ex4/ex4.R
@@ -1,18 +1,26 @@
 ### #################
 ### Read Env Variable
 ### #################
-jid <- Sys.getenv("PBS_JOBID")
-aid <- Sys.getenv("PBS_ARRAYID")
+jid <- paste(Sys.getenv("PBS_JOBID"),
+             Sys.getenv("SLURM_JOB_ID"),
+             sep = ""
+             )
+
+aid <- paste(Sys.getenv("PBS_ARRAYID"),
+             Sys.getenv("SLURM_ARRAY_TASK_ID"),
+             sep = ""
+             )
                                         #
                                         #
 
 ### ##################
-### Print Env Variable
+### SLURM Env Variable
 ### ##################
 cat(paste("This is job: ", jid, ".\n", sep = ""))
 cat(paste("The sub-id is: ", aid, ".\n\n", sep = ""))
                                         #
                                         #
+
 
 ### #########
 ### Use Index
